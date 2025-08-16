dd11f27ae26b9b911fae672449b99241dc41ae04
Admin <admin@Atif-MacBook-Pro.local>
2025-08-16 02:49:40 -0500

    fixed login error

--- library_portal/urls.py
+++ library_portal/urls.py
@@ -19,8 +19,10 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'), 
+    
+    path("", auth_views.LoginView.as_view(template_name="auth/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
 ]
